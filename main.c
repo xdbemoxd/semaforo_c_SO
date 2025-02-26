ac20820cb6ad52c6ea7548af32b5a46bea09f356
Benjamin <benjaminenma99@gmail.com>
2025-02-25 20:57:11 -0400

    actulizacion del generador DATA

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int main()
     //mostrar
     mostrar_LA(aviones);
     mostrar_LV(vuelos);
-    mostrar_LE(equipajes);
+    //mostrar_LE(equipajes);
 
     vaciar_LA(&aviones);
     vaciar_LV(&vuelos);
@@ -59,12 +59,15 @@ void cargarDatos(listaAvion *aviones, listaVuelo *vuelos, listaEquipaje *equipaj
     size_t len = 0;
     ssize_t read;
     int seccion = -1; // -1: ninguna, 0: aviones, 1: vuelos, 2: equipajes
+    int contadorEquipaje=0;
     
     printf("Iniciando carga de datos...\n");
 
-    while ((read = getline(&linea, &len, stdin)) != -1) {
+    while ((read = getline(&linea, &len, stdin)) != -1) 
+    {
         // Eliminar el salto de línea final si existe
-        if (read > 0 && linea[read-1] == '\n') {
+        if (read > 0 && linea[read-1] == '\n') 
+        {
             linea[read-1] = '\0';
         }
         
@@ -73,15 +76,20 @@ void cargarDatos(listaAvion *aviones, listaVuelo *vuelos, listaEquipaje *equipaj
         
         // Detectar sección por comentarios
         if (linea[0] == '#') {
-            if (strstr(linea, "aviones") != NULL) {
+            if (strstr(linea, "aviones") != NULL) 
+            {
                 seccion = 0;
                 printf("\nProcesando aviones...\n");
                 continue;
-            } else if (strstr(linea, "vuelos") != NULL) {
+            } 
+            else if (strstr(linea, "vuelos") != NULL) 
+            {
                 seccion = 1;
                 printf("\nProcesando vuelos...\n");
                 continue;
-            } else if (strstr(linea, "equipajes") != NULL) {
+            } 
+            else if (strstr(linea, "equipajes") != NULL) 
+            {
                 seccion = 2;
                 printf("\nProcesando equipajes...\n");
                 continue;
@@ -89,57 +97,80 @@ void cargarDatos(listaAvion *aviones, listaVuelo *vuelos, listaEquipaje *equipaj
             continue;
         }
 
-        switch(seccion) {
-            case 0: { // Aviones
+        switch(seccion) 
+        {
+            case 0: 
+            { // Aviones
                 avion *nuevoAvion = (avion*)malloc(sizeof(avion));
                 if (sscanf(linea, "%d %d %d", 
                     &nuevoAvion->id, 
                     &nuevoAvion->tipo, 
-                    &nuevoAvion->capacidadCarga) == 3) {
+                    &nuevoAvion->capacidadCarga) == 3) 
+                {
                     insertar_final_LA(aviones, nuevoAvion);
+                    contadorEquipaje++;
                     printf("#");
-                } else {
+                } 
+                else 
+                {
                     free(nuevoAvion);
                     printf("Error al leer avión: %s\n", linea);
                 }
                 break;
             }
-            case 1: { // Vuelos
+            case 1: 
+            { // Vuelos
                 vuelo *nuevoVuelo = (vuelo*)malloc(sizeof(vuelo));
                 if (sscanf(linea, "%d %d %d %d", 
                     &nuevoVuelo->id, 
                     &nuevoVuelo->paisCiudad[0], 
                     &nuevoVuelo->paisCiudad[1], 
-                    &nuevoVuelo->idAvion) == 4) {
+                    &nuevoVuelo->idAvion) == 4) 
+                {
                     insertar_final_LV(vuelos, nuevoVuelo);
                     printf("#");
-                } else {
+                } 
+                else 
+                {
                     free(nuevoVuelo);
                     printf("Error al leer vuelo: %s\n", linea);
                 }
                 break;
             }
-            case 2: { // Equipajes
+            case 2: 
+            { // Equipajes
                 equipaje *nuevoEquipaje = (equipaje*)malloc(sizeof(equipaje));
                 if (sscanf(linea, "%d %d %d %d", 
                     &nuevoEquipaje->id, 
                     &nuevoEquipaje->tipo, 
                     &nuevoEquipaje->vuelo, 
-                    &nuevoEquipaje->fragilidad) == 4) {
+                    &nuevoEquipaje->fragilidad) == 4) 
+                {
                     
                     vuelo *vueloAsignado = buscar_vuelo_por_id(*vuelos, nuevoEquipaje->vuelo);
-                    if (vueloAsignado != NULL) {
+                    if (vueloAsignado != NULL) 
+                    {
                         nuevoEquipaje->paisCiudad[0] = vueloAsignado->paisCiudad[0];
                         nuevoEquipaje->paisCiudad[1] = vueloAsignado->paisCiudad[1];
-                    } else {
+                    } 
+                    else 
+                    {
                         nuevoEquipaje->paisCiudad[0] = 0;
                         nuevoEquipaje->paisCiudad[1] = 0;
                     }
                     
                     nuevoEquipaje->tiempo_inicio = time(NULL);
                     insertar_final_LE(equipajes, nuevoEquipaje);
-                    printf("#");
-                } else {
+
+                    if(contadorEquipaje>0)
+                    {
+                        printf("#");
+                        contadorEquipaje--;
+                    }
+                    
+                } 
+                else 
+                {
                     free(nuevoEquipaje);
                     printf("Error al leer equipaje: %s\n", linea);
                 }
